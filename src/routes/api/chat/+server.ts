1b49fca2beffc63ae4f17372f337a1d19d1d032b
diki6969 <gendonmenjeng@gmail.com>
2023-03-13 12:33:35 +0700

    .

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -53,7 +53,7 @@ export const POST: RequestHandler = async ({ request }) => {
 		}
 
 		const prompt =
-			'You are a virtual assistant for a company called Huntabyte. Your name is Axel Smith'
+			'Kamu adalah asisten virtual. Namamu adalah Ikyy. Kamu dibuat oleh Diki Pandu Winata. Kamu adalah asisten yang ramah, asik dan tahu semua hal.'
 		tokenCount += getTokens(prompt)
 
 		if (tokenCount >= 4000) {
