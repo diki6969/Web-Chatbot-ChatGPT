675556ec0fc976c073c3647d082ed8b3029970c7
diki6969 <gendonmenjeng@gmail.com>
2023-03-21 10:40:28 +0700

    .

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -8,7 +8,7 @@
 	<div class="chat-image avatar">
 		<div class="w-10 rounded-full">
 			<img
-				src="https://telegra.ph/file/{type === 'user' ? 'd50e5008e0f3753717faa.png' : 'ce72a789d7fee93959e58.jpg'}"
+				src="https://telegra.ph/file/{type === 'user' ? '4463acde836734a2dd80e.png' : 'ce72a789d7fee93959e58.jpg'}"
 				alt="{type} avatar"
 			/>
 		</div>
