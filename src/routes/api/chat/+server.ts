6b55d78fa117ecc5cb80a3b6d93e4fdf89df0b78
IkyyOfficial <gendonmenjeng@gmail.com>
2023-04-06 05:23:59 +0700

    update src/routes/api/chat/+server.ts

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -4,6 +4,7 @@ import type { RequestHandler } from './$types'
 import { getTokens } from '$lib/tokenizer'
 import { json } from '@sveltejs/kit'
 import type { Config } from '@sveltejs/adapter-vercel'
+import fetch from 'node-fetch';
 
 export const config: Config = {
 	runtime: 'edge'
